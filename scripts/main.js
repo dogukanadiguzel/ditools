76e88a8c744f89436d54c290548759ed9bbc1660
Followersmanager <followersmanager@gmail.com>
2018-01-08 19:26:23 +0300

    my initial commit

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+
+// for(i=0;i<15;i++){}
+getselectvalue1();
+function getselectvalue1(){
+	var selectedvalue=document.getElementById("question1").value;
+	console.log(selectedvalue);
+}
+getselectvalue2();
+function getselectvalue2(){
+	var selectedvalue=document.getElementById("question2").value;
+	console.log(selectedvalue);
+}
